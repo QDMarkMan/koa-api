94a65437c8174cc0b20dfd8ead092b2bda68ec5f
unknown <5231202571@qq.com>
2018-11-02 13:31:03 +0800

    完成增删改查

--- config/log.config.js
+++ config/log.config.js
@@ -1,1 +1,8 @@
+/*
+ * @Author: etongfu 
+ * @Date: 2018-11-02 13:30:45 
+ * @Last Modified by:   etongfu 
+ * @Last Modified time: 2018-11-02 13:30:45 
+ * log4js配置文件
+ */
 const path = require('path')
